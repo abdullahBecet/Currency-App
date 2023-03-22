3109a438dc1a83622fe57938904b11fca5fd093c
Abdullah BECET <apobeetgmail.com>
2023-03-22 16:35:14 +0300

    first commit

---
+++ Currency Converter.py
@@ -0,0 +1,32 @@
+from requests import get
+from pprint import PrettyPrinter
+BASE_URL= "https://free.currconv.com/"
+API_KEY = "562ddaf40c95f5d58108"
+
+printer = PrettyPrinter()
+def get_currencies():
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()['results']
+
+    dala = list(data.items())
+
+    data.sort()
+
+    return data
+
+def print_currencies(currencies):
+    for name, currency in currencies:
+        name = currency['currenciyName']
+        _id = currency['id']
+        symbol = currency.get("currencySymbol","")
+        print(f"{_id} - {name} - {symbol}")
+
+
+
+
+data = get_currencies()
+printer.pprint(data)
+
+
+
