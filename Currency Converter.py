687f3ca245e138817b3a2d84745080bb0b09ab71
Abdullah BECET <apobeetgmail.com>
2023-03-22 18:20:42 +0300

    Main 1

--- Currency Converter.py
+++ Currency Converter.py
@@ -51,8 +51,33 @@ def convert(currency1, currency2, amount):
     print(f"{amount} {currency1} is equal to {converted_amount} {currency2}")
     return converted_amount
 
+def main():
+    currencies = get_currencies()
+    print("Welcome to the currency converter!")
+    print("List - lists the different currencies")
+    print("Convert - convert from one currency to another")
+    print("Rate - get the exchange rate of two currencies")
+    print()
 
+    while True:
+        command = input("Enter a command (q to quit): ").lower():
 
+        if command == "q":
+            break
+        elif command == "list":
+            print_currencies(currencies)
+        elif command == "convert":
+            currency1 = input("Enter a base currency name: ").upper()
+            amount = input("Enter a amount name: ").upper()
+            currency2 = input("Enter a currency to convert name: ").upper()
+            convert(currency1, currency2, amount)
+        elif command == "rate":
+            currency1 = input("Enter a base currency name: ").upper()
+            currency2 = input("Enter a currency to convert name: ").upper()
+            exchange_rate(currency1, currency2)
+        else:
+            print("Unrecognized command!")
 
+main()
 
 
